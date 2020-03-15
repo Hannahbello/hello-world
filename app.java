1083ef266bc846b8f41dd0a12a086758b8a0af68
Hannahbello <bellohannah28@gmail.com>
2020-03-14 17:14:52 -0700

    i want to edit the project

--- app.java
+++ app.java
@@ -1,7 +1,8 @@
 package com.company;
 
-public class Main {
+public class Program {
 
     public static void main(String[] args) {
         System.out.println("my name is hannah");
+        System.out.println("i am a java developer");
     }
